b0e5be158b5d4564c8fa80fe72a9d60f42e22019
Navneet Ranjan <mail.navneetranjan@gmail.com>
2023-07-09 14:59:11 +0530

    db.py

--- config/db.py
+++ config/db.py
@@ -1,15 +1,9 @@
 from sqlalchemy import create_engine,MetaData
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+
 
 
 
 engine = create_engine("mysql+pymysql://root@localhost:3309/test")
-
-SessionLocal = sessionmaker(autocommit= False, autoflush=False,bind=engine)
-
-Base = declarative_base()
-
 
 meta = MetaData()
 conn = engine.connect()
