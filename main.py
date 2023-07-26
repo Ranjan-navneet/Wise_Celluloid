95ac1b1b74c5e1632259ee98c60a63a291e0abf4
Navneet-07 <mail.navneetranjan@gmail.com>
2023-07-26 16:30:36 +0530

    SMTP protocol

--- main.py
+++ main.py
@@ -1,6 +1,64 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request ,Form
 from routes.index import user
+# import pyotp
+# import time
+# import qrcode
+from starlette.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+templates = Jinja2Templates(directory="templates")
+
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static",html=True),name="static")
 
 app.include_router(user)
 
+# key = pyotp.random_base32()
+
+# -------- Code for the time based one time password (TOTP) ---------
+# totp = pyotp.TOTP(key)
+
+# print(totp.now())
+# input_code = input("Enter 2FA Code: ")
+
+# print(totp.verify(input_code))
+# if(totp.verify(input_code)==1):
+#     print("Successfully verified")
+# else:
+#     print("No")
+# time.sleep(30)
+# print(totp.now())
+
+# -------- Code for the Hashed based message authenctication Code (HOTP) ---------
+# Counter =0
+# hotp = pyotp.HOTP(key)
+# print(hotp.at(0))
+
+# Hashed_OTP = hotp.verify(input("Enter Code: "),Counter)
+# for Counter in range(1):
+    # if(Hashed_OTP==True):
+        # print("Successful")
+    # else:
+        # print("Try Again")
+        # Counter+=1 
+
+#---------- QR CODE Based Authentication --------------
+
+# uri = pyotp.totp.TOTP(key).provisioning_uri(name="Cristiano", issuer_name="Wise Celluloid")
+
+# print(uri)
+
+# qrcode.make(uri).save("name.png")
+
+# totp = pyotp.TOTP(key)
+# QR_CODE = totp.verify(input("Enter Code: "))
+
+# if(QR_CODE == True):
+#     print("Successfully scanned")
+# else:
+#     print("Error!") 
+    
+
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
--- routes/user.py
+++ routes/user.py
@@ -1,17 +1,60 @@
+from typing import Union
+from fastapi import FastAPI, Request , Form
 from fastapi import APIRouter
 from config.db import conn
 from models.index import users
 from schemas.index import User
+from starlette.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import smtplib
+from email.message import EmailMessage
+
+# import pyotp
+# import time
+# import qrcode
+
 
 user = APIRouter()
 data=[]
 
+templates = Jinja2Templates(directory="templates")
+user.mount("/static", StaticFiles(directory="static",html=True),name="static")
+
 @user.get("/")
-async def read_data():
-   return {"hellooooo"}
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
+@user.post("/submit")
+def submit(name: str = Form(),emailAddress:str=Form(),message:str=Form()):
+    print(name)
+    print(emailAddress)
+    print(message)
+    
+    email_address = "navneetranjan800@gmail.com" #type mail address
+    email_password = "czfvkronuhhurwzd"
+    
+    #create email
+    msg = EmailMessage()
+    msg['Subject'] = "Do.not.reply"
+    msg['From'] = email_address
+    msg['To'] = "vv7097@srmist.edu.in" #type mail id
+    msg.set_content(
+    f"""\
+        Name : {name}
+        Email: {emailAddress}
+        Message: {message}
+        """,
+    )
+    
+   #send email
+    with smtplib.SMTP_SSL('smtp.gmail.com',465) as smtp:
+        smtp.login(email_address,email_password)
+        smtp.send_message(msg) 
+    
+    return "Successfully sent"
+   
 
-@user.post("/")
+@user.post("/write")
 async def write_data(user: User):
     data.append(user.dict())
     return data
@@ -31,3 +74,19 @@ async def update_data(id: int ,user: User):
 async def delete_data(id : int):
     data.pop(id)
     return data
+
+# key = pyotp.random_base32()
+
+# # -------- Code for the time based one time password (TOTP) ---------
+# totp = pyotp.TOTP(key)
+
+# print(totp.now())
+# input_code = input("Enter 2FA Code: ")
+
+# print(totp.verify(input_code))
+# if(totp.verify(input_code)==1):
+#     print("Successfully verified")
+# else:
+#     print("No")
+# time.sleep(30)
+# print(totp.now())
